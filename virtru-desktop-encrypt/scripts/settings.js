6d86d2663035e9952d3acd481e740e00ea44ba32
Trevor Foskett <trevor@virtru.com>
2020-02-27 13:34:40 -0500

    Update settings.js

--- virtru-desktop-encrypt/scripts/settings.js
+++ virtru-desktop-encrypt/scripts/settings.js
@@ -5,14 +5,11 @@ const fs = require('fs');
 
 var emailInput = document.getElementById('email-address-settings');
 var appIdInput = document.getElementById('appid-settings');
-var codeInputArea = document.getElementById('code-input-block');
-var codeInputField = document.getElementById('verification-code-input');
-var codeInputPlaceholder = document.getElementById('code-input-placeholder');
 
-var codeInputTooltip = document.getElementById('code-input-tooltip-text');
 
-var reqAppIdBtn = document.getElementById('appid-gen');
-var submitCodeBtn = document.getElementById('code-submit');
+
+
+
 var saveExitBtn = document.getElementById('save-close');
 var selectSaveDirBtn = document.getElementById('select-save-location');
 
@@ -22,27 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
   ipcRenderer.send('settings-page-loaded');
 })
 
-reqAppIdBtn.addEventListener('click', (event) => {
-  codeInputArea.style.display = 'block';
-  reqAppIdBtn.style.display = 'none';
-  submitCodeBtn.style.display = 'block';
-  codeInputPlaceholder.style.display = 'none';
-  codeInputTooltip.innerText = `A verification code has just been sent to ${emailInput.value}. Enter that code here.`
-  ipcRenderer.send('app-id-request', [
-    emailInput.value
-  ]);
-})
 
-submitCodeBtn.addEventListener('click', (event) => {
-  submitCodeBtn.style.display = 'none';
-  reqAppIdBtn.style.display = 'block';
-  codeInputArea.style.display = 'none';
-  codeInputPlaceholder.style.display = 'block';
-  ipcRenderer.send('code-submit', [
-    emailInput.value,
-    codeInputField.value
-  ]);
-})
+
+
 
 selectSaveDirBtn.addEventListener('click', (event) => {
   ipcRenderer.send('save-directory-clicked');
